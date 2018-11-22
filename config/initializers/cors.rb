93229c123d078d2bc6d1970481255ff381330863
Casey Van-Roose <caseyvanroose@Caseys-MacBook-Pro.local>
2018-11-22 10:48:30 +0000

    changed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    #'https://www.pokemon-compare.com'
+    origins 'https://www.pokemon-compare.com'
+    
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
